36ac0d93fc229c34d7fc7ac515f625c0c6e4450f
YJY-G <3620864089@qq.com>
2025-02-28 15:48:49 +0000

    new update

--- backend/properties/urls.py
+++ backend/properties/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path  
-from .views import AmentityListCreateAPIView,HotelUploadByOwner,HotelListView, HotelDetailViewById,HotelDetailViewByOnwer
+from .views import AmentityListAPIView,HotelListAPIView,HotelDetailAPIView
 
 urlpatterns = [  
-    path('hotels/upload', HotelUploadByOwner.as_view(), name='hotel-list-create'),
-    path('amentities/', AmentityListCreateAPIView.as_view(), name='amentity-list-create'),
-    path('hotels/list/', HotelListView.as_view(), name='hotel-list'),
-    path('hotels/detail/<int:pk>/', HotelDetailViewById.as_view(), name='hotel-detail'),
-    path('hotels/detail/owner/<int:pk>/', HotelDetailViewByOnwer.as_view(), name='hotel-detail-by-owner'),
+    path('amentities/', AmentityListAPIView.as_view(), name='amentity-list-create'),
+    path('hotels/list/', HotelListAPIView.as_view(), name='hotel-list'),
+    path('hotels/detail/<int:pk>/', HotelDetailAPIView.as_view(), name='hotel-detail'),
 ] 
